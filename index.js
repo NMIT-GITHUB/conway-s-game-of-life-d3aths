6eeb704755e1da6604429ccd162cd034777f027c
Celeste <celeste-quinn@live.nmit.ac.nz>
2020-06-10 09:04:12 +1200

    class acitivities

---
+++ index.js
@@ -0,0 +1,24 @@
+let row1 = [0,0,0]
+let row2 = [1,0,1]
+let row3 = [0,1,0]
+let hasN
+
+function gameplay() {
+
+row1[0] if row1[1] && row2[0] && row2[1] == 1
+        row1[0]  = 3 neighbours
+
+row1[0] if row1[1] && row2[0] && row2[1] == 0
+        row1[0]  = 0 neighbours
+
+row1[0] if row1[1] == 1 && row2[0] && row2[1] == 0
+        row1[0]  = 1 neighbours
+
+row1[0] if row1[1] && row2[0] == 1 && row2[1] == 0
+        row1[0]  = 2 neighbours
+
+   //this is bullshit cause it would have to write like a hundred lines going through every index checking
+}
+console.log(' ' + row1 +'\n', row2 + '\n', row3 + '\n')
+gameplay()
+console.log(' ' + row1 +'\n', row2 + '\n', row3 + '\n')
